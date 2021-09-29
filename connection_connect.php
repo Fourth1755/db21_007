690c54e49939987e28af84cffbca07c519c1dbcf
Fourth1755 <fourthe.hero@gmail.com>
2021-09-29 23:09:50 +0700

    ภาษาไทย

--- connection_connect.php
+++ connection_connect.php
@@ -4,8 +4,8 @@
     $username = "db21_007";
     $password ="db21_007";
     $dbname = "db21_007";
-
     $conn = mysqli_connect($severname,$username,$password);
+    mysqli_set_charset($conn, "utf8");
     if($conn->connect_error){
         die("Connection failed: ".$conn->connect_error);
     }
--- views/order/newOrder.php
+++ views/order/newOrder.php
@@ -13,7 +13,7 @@
         <form method="get" action="" style="width: 40%; ">
     <div class="mb-3">
         <label for="formGroupExampleInput" class="form-label">Quotation_ID</label>
-        <input type="text" class="form-control" name= "name" placeholder="Anime input placeholder" > 
+        <input type="text" class="form-control" name= "name" placeholder="ID" > 
     </div>
     <div class="mb-3">
         <label for="formGroupExampleInput2" class="form-label">Quotation_Date</label>
