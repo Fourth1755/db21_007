3ab2dc2798f260ca7d198740a53af69cdfbd06a0
Fourth1755 <fourthe.hero@gmail.com>
2021-09-29 22:25:43 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,9 +1,5 @@
 <?php
-<<<<<<< HEAD
-    $controllers =array('pages'=>['home', 'error'], 'order'=>['index','newOrder','employee'], 'rate'=>['index']);
-=======
-    $controllers =array('pages'=>['home', 'error'], 'order'=>['index','newOrder'],'orderdetail'=>['index','newOrder'], 'rate'=>['index']);
->>>>>>> f903b3be6368cbb0129bf67b679b4d0d772c9d78
+    $controllers =array('pages'=>['home', 'error'], 'order'=>['index','newOrder','employee'], 'orderdetail'=>['index','newOrder'],'rate'=>['index']);
     function call($controller, $action){
         //echo "routes to".$controller."-".$action."<br>";
         require_once("controllers/".$controller."_controller.php");
