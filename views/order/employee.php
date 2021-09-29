2a67f3989f6d291d28098a124d9c1edb7e5da903
Fourth1755 <fourthe.hero@gmail.com>
2021-09-29 22:33:42 +0700

    Update employee.php

--- views/order/employee.php
+++ views/order/employee.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,9 +8,9 @@
 </head>
 <body >
     <div class="container">
-    <nav class="navbar navbar-expand-lg navbar-light bg-dark">
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Order</a>
+    <a class="navbar-brand" href="?controller=order&action=index">Order</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
