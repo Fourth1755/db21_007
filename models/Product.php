2731a86ccbb7860047908f1b759de1fb7890f38f
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-09-29 11:50:27 +0700

    Create Product.php

---
+++ models/Product.php
@@ -0,0 +1,17 @@
+<?php
+class Product{
+    public $Product_ID
+    public $Product_Name
+    public $Product_Detail
+    public $Category_ID
+    public $Product_Stock
+}
+
+public function__construct($Product_ID, $Product_Name, $Product_Detail, $Category_ID, $Product_Stock){
+    $this->Product_ID = $Product_ID
+    $this->Prodcut_Name = $Product_Name
+    $this->Product_Detail = $Product_Detail
+    $this->Category_ID = $Category_ID
+    $this->$Product_Stock = $Product_Stock
+}
+?>
