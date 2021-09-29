30da6b84c0b97d0e31e08f4a153bb88a95594186
Fourth1755 <fourthe.hero@gmail.com>
2021-09-29 22:24:03 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,5 +1,9 @@
 <?php
+<<<<<<< HEAD
     $controllers =array('pages'=>['home', 'error'], 'order'=>['index','newOrder','employee'], 'rate'=>['index']);
+=======
+    $controllers =array('pages'=>['home', 'error'], 'order'=>['index','newOrder'],'orderdetail'=>['index','newOrder'], 'rate'=>['index']);
+>>>>>>> f903b3be6368cbb0129bf67b679b4d0d772c9d78
     function call($controller, $action){
         //echo "routes to".$controller."-".$action."<br>";
         require_once("controllers/".$controller."_controller.php");
@@ -10,6 +14,9 @@
                         require_once("models/employeeModels.php");
                         require_once("models/customerModels.php");
                         $controller = new OrderController();
+                        break;
+            case "orderdetail": require_once("models/orderdetailModels.php");
+                        $controller = new OrderdetailController();
                         break;
             case "rate": require_once("models/rateModels.php");
                         $controller = new rateController();
