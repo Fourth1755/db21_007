5e0e7b8ad2f4ef8c2c6d5d851830476bc892f8e2
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-09-29 15:30:05 +0700

    Update routes.php

--- routes.php
+++ routes.php
@@ -1,30 +1,30 @@
 <?php
-    $controllers =array('pages'=>['home','error'],'order'=>['index'], 'rate'=>['index']);
-    function call($controller,$action){
+    $controllers =array('pages'=>['home', 'error'], 'order'=>['index'], 'rate'=>['index']);
+    function call($controller, $action){
         //echo "routes to".$controller."-".$action."<br>";
         require_once("controllers/".$controller."_controller.php");
         switch($controller){
-            case "pages": $controller=new PagesController();
+            case "pages": $controller = new PagesController();
                         break;
             case "order": require_once("models/orderModels.php");
                         require_once("models/employeeModels.php");
-                        $controller=new OrderController();
+                        $controller = new OrderController();
                         break;
             case "rate": require_once("model/rateModel.php");
-                        $controller=new OrderController();
+                        $controller = new OrderController();
                         break;
         }
         $controller->{$action}();
     }
     if(array_key_exists($controller,$controllers)){
-        if(in_array($action,$controllers[$controller])){
-            call($controller,$action);
+        if(in_array($action, $controllers[$controller])){
+            call($controller, $action);
         }
         else{
-            call('pages','error');
+            call('pages', 'error');
         }   
     } 
     else{
-        call('pages','error');
+        call('pages', 'error');
     }
 ?>
