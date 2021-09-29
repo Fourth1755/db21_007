5aaf87b973ea3bf57d9a0c46ee4e646a8f7dda92
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-09-29 19:06:51 +0700

    Create orderdetail_controller.php

---
+++ controllers/orderdetail_controller.php
@@ -0,0 +1,27 @@
+<?php
+     class OrderdetailController{
+        public function index(){
+            $Orderdetail=Orderdetail::getAll();
+            require_once('views/Orderdetail/index_Orderdetail.php');
+        }
+        public function newOrderdetail(){
+
+            require_once('views/Orderdetail/Orderdetail.php');
+        }
+        public function addOrderdetail(){
+            $Orderdetail_ID=$_GET['Orderdetail_ID'];
+            $Order_ID=$_GET['Order_ID'];
+            $Colordetail_ID=$_GET['Colordetail_ID'];
+            $Orderdetail_QuantityColor=$_GET['Orderdetail_QuantityColor'];
+            $Orderdetail_Quantity=$_GET['Orderdetail_Quantity'];
+            Orderdetail::add($Orderdetail_ID,$Order_ID,$Colordetail_ID,$Orderdetail_QuantityColor,$Orderdetail_Quantity);
+            OrderdetailController::index();
+        }
+        public function updateForm(){
+            $Orderdetail_ID=$_GET['Orderdetail_ID'];
+            $Orderdetail=Orderdetail::get($Orderdetail_ID);
+            //$studio_list=Studio::getAll();
+            require_once('views/Orderdetail/updateForm.php');
+        }
+    }
+?>
