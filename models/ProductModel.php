a6d0f365a6d8f4d6df92b9e5ad5544a96483d75d
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-09-29 12:49:44 +0700

    Update ProductModel.php

--- models/ProductModel.php
+++ models/ProductModel.php
@@ -8,11 +8,11 @@ class productModel{
 }
 
 public function__construct($Product_ID, $Product_Name, $Product_Detail, $Category_ID, $Product_Stock){
-    $this->Product_ID = $Product_ID
-    $this->Prodcut_Name = $Product_Name
-    $this->Product_Detail = $Product_Detail
-    $this->Category_ID = $Category_ID
-    $this->$Product_Stock = $Product_Stock
+    $this->Product_ID = $Product_ID;
+    $this->Prodcut_Name = $Product_Name;
+    $this->Product_Detail = $Product_Detail;
+    $this->Category_ID = $Category_ID;
+    $this->$Product_Stock = $Product_Stock;
 }
 
 public static function get($Product_ID){
