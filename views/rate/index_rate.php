dc12a1a3288b25e75af70f25cda6a5ed315f3142
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-09-29 15:13:34 +0700

    Create index_rate.php

---
+++ views/rate/index_rate.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rate</title>
+</head>
+<body >
+    <div class="container">
+        <h1>Rate</h1>
+        <table class="table">
+        <tr><td>Product_ID</td><td>Rate_ID</td><td>Rate_Quantity</td><td>Rate_Price</td><td>Rate_ScreenPrice</td>
+        <td>Update</td><td>Delete</td></tr>
+        <?php
+            foreach($order_list as $order){
+            echo "<tr><td>$order->id</td>
+                <td>$Rate->Product_ID</td>
+                <td>$Rate->Rate_ID</td>
+                <td>$Rate->Rate_Quantity</td>
+                <td>$Rate->Rate_Price</td>
+                <td>$Rate->Rate_ScreenPrice</td>
+                <td><a href=?controller=order&action=updateForm&orderID=$Rate->Rate_ID>Update</a></td>
+                <td>Delete</td></tr>";
+            }    
+            echo "</table>";
+        ?>
+    <!--<a class= "btn btn-success" href=?controller=rate&action=newOrder>add new</a>-->
+    </div>
+    
+    
+    
+</body>
+</html>
