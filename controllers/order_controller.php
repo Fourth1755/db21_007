5b4f0ab11a8b97a64ab65f8da12b74cd01b5fa8f
Fourth1755 <fourthe.hero@gmail.com>
2021-09-30 20:13:59 +0700

    Update order_controller.php

--- controllers/order_controller.php
+++ controllers/order_controller.php
@@ -13,7 +13,7 @@
             $customer_list=Customer::getAll();
             require_once('views/order/newOrder.php');
         }
-        public function addOrder($id,$date,$sellerID,$customerID,$deposit,$managerID,$dateApprov,$extraProduct,$dateMenufacture,$transmissionStatus){
+        public function addOrder(){
             $id=$_GET["id"];
             $date=$_GET["date"];
             $sellerID=$_GET["sellerID"];
