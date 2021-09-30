5deaaaad6544abe6d8cc8441042ccc44291cd61d
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-09-30 22:28:22 +0700

    add Rate -> select Product

--- controllers/rate_controller.php
+++ controllers/rate_controller.php
@@ -4,5 +4,10 @@
             $rateList = Rate::getAll();
             require_once('views/rate/index_rate.php');
         }
+
+        public function addRate(){
+            $productList = Product::getAll();
+            require_once('views/rate/addRate.php');
+        }
     }
 ?>
--- views/rate/addRate.php
+++ views/rate/addRate.php
@@ -4,9 +4,18 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Add New Rate</title>
 </head>
 <body>
-    Hello World!!
+    <form>
+    <label>Product_Name <select>
+        <?php foreach($productList as $Product){echo "option values = $Product->Product_ID>$Product->Product_Name</option>";}?>
+    </select></label>
+
+
+
+
+
+    </form>
 </body>
 </html>
