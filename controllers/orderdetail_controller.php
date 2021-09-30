3e6dab5ca1918ae5dd898a36c3884fbfe98eff32
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-10-01 00:55:11 +0700

    Update orderdetail_controller.php

--- controllers/orderdetail_controller.php
+++ controllers/orderdetail_controller.php
@@ -1,28 +1,26 @@
 <?php
      class OrderdetailController{
         public function index(){
-            $orderdetail_list=Orderdetail::getAll();
+            $order_list = Orderdetail::getAll();
             require_once('views/orderdetail/index_orderdetail.php');
         }
         public function newOrderdetail(){
-            $orderdetail_list = Orderdetail::getAll();
-            require_once('views/orderdetail/Orderdetail.php');
+            $productList = Product::getAll();
+            require_once('views/orderdetail/newOrderdetail.php');
         }
+
         public function addOrderdetail(){
-            $Orderdetail_ID=$_GET['Orderdetail_ID'];
-            $Order_ID=$_GET['Order_ID'];
-            $Colordetail_ID=$_GET['Colordetail_ID'];
-            $Orderdetail_QuantityColor=$_GET['Orderdetail_QuantityColor'];
-            $Orderdetail_Quantity=$_GET['Orderdetail_Quantity'];
-            Orderdetail::add($Orderdetail_ID,$Order_ID,$Colordetail_ID,$Orderdetail_QuantityColor,$Orderdetail_Quantity);
+            $Orderdetail_ID=$_GET ['Orderdetail_ID'];
+            $Order_ID=$_GET ['Order_ID'];
+            $Colordetail_ID=$_GET ['Colordetail_ID'];
+            $Orderdetail_QuantityColor=$_GET ['Orderdetail_QuantityColor'];
+            $Orderdetail_Quantity=$_GET ['Orderdetail_Quantity'];
+
+            Orderdetail::add($Orderdetail_ID, $Order_ID,  $Colordetail_ID, $Orderdetail_QuantityColor,$Orderdetail_Quantity);
 
             OrderdetailController::index();
+
         }
-        public function updateForm(){
-            $Orderdetail_ID=$_GET['Orderdetail_ID'];
-            $orderdetail=Orderdetail::get($Orderdetail_ID);
-            //$studio_list=Studio::getAll();
-            require_once('views/orderdetail/updateForm.php');
-        }
+        
     }
 ?>
