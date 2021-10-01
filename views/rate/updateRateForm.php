eddbf2f57be80836a35e759be87d9309b45d1579
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-10-01 11:28:17 +0700

    Update updateRateForm.php

--- views/rate/updateRateForm.php
+++ views/rate/updateRateForm.php
@@ -13,12 +13,15 @@
                 echo "<option values = $Product->Product_ID>$Product->Product_Name</option>";
                 }
             ?>
-        </label>
-        <label>Rate_ID<input type="text" name="Rate_ID" value="<?php echo $Rate->Rate_ID;?>"></label>
-        <label>Rate_Quantity<input type="text" name="Rate_Quantity" value="<?php echo $Rate->Rate_Quantity;?>"></label>
-        <label>Rate_Price<input type="text" name="Rate_Price" value="<?php echo $Rate->Rate_Price;?>"></label>
-        <label>Rate_ScreenPrice<input type="text" name="Rate_ScreenPrice" value="<?php echo $Rate->Rate_ScreenPrice;?>"></label>
+        </label><br>
+        <label>Rate_ID<input type="text" name="Rate_ID" value="<?php echo $Rate->Rate_ID;?>"></label><br>
+        <label>Rate_Quantity<input type="text" name="Rate_Quantity" value="<?php echo $Rate->Rate_Quantity;?>"></label><br>
+        <label>Rate_Price<input type="text" name="Rate_Price" value="<?php echo $Rate->Rate_Price;?>"></label><br>
+        <label>Rate_ScreenPrice<input type="text" name="Rate_ScreenPrice" value="<?php echo $Rate->Rate_ScreenPrice;?>"></label><br>
         
+        <input type="hidden" name="controller" value="rate"/>
+        <button type="submit" name="action" value="index_rate">Back</button>
+        <button type="submit" name="action" value="update">Update</button>
     </form>
 </body>
 </html>
