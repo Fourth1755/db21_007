c93889d94348caa34b6a4fad1cc178d0f8085b70
Fourth1755 <fourthe.hero@gmail.com>
2021-10-02 15:16:17 +0700

    Update orderModels.php

--- models/orderModels.php
+++ models/orderModels.php
@@ -111,8 +111,7 @@
             LEFT JOIN(SELECT Quotation_ID,Employee_ID,Employee_Name AS Manager_Name FROM Quotation LEFT JOIN Employee ON Quotation.Manager_ID=Employee.Employee_ID) AS a 
             ON Quotation.Quotation_ID=a.Quotation_ID LEFT JOIN Employee ON Quotation.Seller_ID=Employee.Employee_ID 
             LEFT JOIN Customer ON Quotation.Customer_ID=Customer.Customer_ID
-            WHERE (Quotation_ID LIKE '%$key' OR Quotation_Date LIKE '%$key' OR Seller_ID LIKE '%$key' OR Seller_Name LIKE '%$key'
-            OR Customer_ID LIKE '%$key' OR Customer_Name LIKE '%$key' OR Quotation_Deposit LIKE '$key')";
+            WHERE (Quotation_ID LIKE '%$key' OR Quotation_Date LIKE '%$key')";
             $result=$conn->query($sql);
             while($my_row = $result->fetch_assoc()){
                 $id=$my_row["Quotation_ID"];
