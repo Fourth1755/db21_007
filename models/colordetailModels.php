5426e74f651dddd799a3f5e54f6463ed7593f072
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-10-02 20:29:36 +0700

    Update colordetailModels.php

---
+++ models/colordetailModels.php
@@ -0,0 +1,26 @@
+<?php
+    class Colordetail{
+        public $Product_ID;
+        public $Colordetail_ID;
+        public $Color_Name;
+        public function __construct($Product_ID,$Colordetail_ID,$Color_Name){
+            $this->Product_ID=$Product_ID;
+            $this->Colordetail_ID=$Colordetail_ID;
+            $this->Color_Name=$Color_Name;
+        }
+        public static function getAll(){
+            $colordetailList=[];
+            require("connection_connect.php");
+            $sql="SELECT * FROM ColorDetail";
+            $result=$conn->query($sql);
+            while($my_row = $result->fetch_assoc()){
+                $Product_ID=$my_row["Product_ID"];
+                $Colordetail_ID=$my_row["ColorDetail_ID"];
+                $Color_Name=$my_row["Color_Name"];
+                $customerList[]=new Customer($Product_ID,$Colordetail_ID,$Color_Name);
+            }
+            require("connection_close.php");
+            return $colordetailList;
+        }
+    }
+?>
