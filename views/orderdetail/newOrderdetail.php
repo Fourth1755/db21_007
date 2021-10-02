84e2e4ebe1994a7703852c611be6b17d75fd6da9
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-10-03 02:31:46 +0700

    Update newOrderdetail.php

--- views/orderdetail/newOrderdetail.php
+++ views/orderdetail/newOrderdetail.php
@@ -5,45 +5,53 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add New Orderdetail</title>
-    </head>
-<body>
-
-    <div class="container">
-    <h1>Add New Orderdetail</h1>
-    <form method="get" action="" style="width: 40%; ">
-    <div class="mb-3">
-        <label for="formGroupExampleInput" class="form-label">Orderdetail_ID</label>
-        <input type="text" class="form-control" name= "Colordetail_ID" placeholder="Ex COLOR011"> 
-    </div>
-    <div class="mb-3">
-        <label for="formGroupExampleInput2" class="form-label">Order_ID</label>
-        <input type="date" class="form-control" name= "Order_ID" placeholder="Ex 11001">
-    </div>
-    <div class="mb-3">
-        <label for="formGroupExampleInput2" class="form-label">Seller
-            <select name="sellerID" class="form-select" aria-label="Default select example">
-            <option selected disabled hidden>Open this select Seller</option>
-                <?php
-                    foreach($employee_list as $sell){
-                        if($sell->positionID=='POS01'){
-                            echo"<option value=$sell->id>$sell->name</option>";
-                        }    
-                    }    
-                ?>
 </head>
 <body>
-<form>
-    <label>ColorDetail<select name="ColorDetail_ID">
-        <?php foreach($productList as $Product){
-            echo "<option values = $Product->Product_ID>$Product->Product_Name</option>";
-            }
-        ?>
-    </select></label><br>
-    <label>Orderdetail_ID <input type="text" name="Orderdetail_ID"/></label><br>
-    <label>Order_ID <input type="text" name="Order_ID"/>></label><br>
-    <label>ColorDetail_ID <input type="text" name="ColorDetail_ID"/>></label><br>
-    <label>OrderDetail_QuantityColor <input type="text" name="OrderDetail_QuantityColor"/>></label><br>
-    <label>OrderDetail_Quantity	 <input type="text" name="OrderDetail_Quantity"/>></label><br>
+    
+    <div class="container">
+        <h1>Add New Orderdetail</h1>
+        <form method="get" action="" style="width: 40%; ">
+    <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">Orderdetail_ID</label>
+        <input type="text" class="form-control" name= "Orderdetail_ID" placeholder=" Ex QD001">
+    </div>
+    <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">Order_ID
+            <select name="Order_ID" class="form-select" aria-label="Default select example">
+            <option selected disabled hidden>Open this select Order_ID</option>
+                <?php
+                    foreach($ProductList as $product){
+                        if($product->Product_ID=='PR001'){
+                        echo"<option value=$product->Product_ID>$product->Product_Name</option>";
+                    }    
+                } 
+                ?>
+            </select>
+        </label>
+    </div>
+    <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">Colordetail_ID
+            <select name="Colordetail_ID" class="form-select" aria-label="Default select example">
+            <option selected disabled hidden>Open this select Colordetail_ID</option>
+                <?php
+                         foreach($colordetail_list as $color){
+                            if($color->Colordetail_ID=='COLOR011'){
+                                echo"<option value=$color->Colordetail_ID>$color->Color_Name</option>";
+                            }    
+                        }   
+                ?>
+            </select>
+        </label>
+    </div>
+    <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">Orderdetail_QuantityColor</label>
+        <input type="int" class="form-control" name= "Orderdetail_QuantityColor" placeholder=" Ex 1">
+    </div>
+    <div class="mb-3">
+        <label for="formGroupExampleInput2" class="form-label">Orderdetail_Quantity</label>
+        <input type="int" class="form-control" name= "Orderdetail_Quantity" placeholder=" Ex 500">
+    </div>
+    <br>
     <input type="hidden" name="controller" value="Orderdetail"/>
     <button type="submit" name="action" value="index">Back</button>
     <button type="submit" name="action" value="addOrderdetail">Save</button>
