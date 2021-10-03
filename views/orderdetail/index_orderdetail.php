8f45d1d76dbeafd7c00419cb637e05e8e528ad21
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-10-03 15:25:14 +0700

    Update index_orderdetail.php

--- views/orderdetail/index_orderdetail.php
+++ views/orderdetail/index_orderdetail.php
@@ -7,25 +7,33 @@
     <title>Orderdetail</title>
 </head>
 <body >
-    <div class="container">
+<div class="container">
         <h1>Orderdetail</h1>
+        <form method="get"action="">
+            <input type="text" name="key">
+            <input type="hidden" name="controller" value="orderdetail">
+            <button type="submit" name="action" value="search">Search</button>
+        </form>
         <table class="table">
-        <tr><td>Orderdetail_ID</td><td>Order_ID</td><td>Colordetail_ID</td><td>Orderdetail_QuantityColor</td><td>Orderdetail_Quantity</td>
-        <td>Update</td><td>Delete</td></tr>
+        <tr><td>Orderdetail_ID</td><td>Order_ID</td><td>Colordetail_ID</td><td>Orderdetail_QuantityColor</td><td>Orderdetail_Quantity</td><td>Update</td><td>Delete</td></tr>
         <?php
-            foreach($orderdetail_list as $orderdetail){
+            foreach($orderdetailList as $orderdetail){
             echo "<tr>
                 <td>$orderdetail->Orderdetail_ID</td>
                 <td>$orderdetail->Order_ID</td>
                 <td>$orderdetail->Colordetail_ID</td>
                 <td>$orderdetail->Orderdetail_QuantityColor</td>
                 <td>$orderdetail->Orderdetail_Quantity</td>
-                <td><a href=?controller=orderdetail&action=updateForm&Orderdetail_ID=$orderdetail->Orderdetail_ID>Update</a></td>
-                <td>Delete</td></tr>";
-            }    
-            echo "</table>";//<td>$orderdetail->orderdetail_ID</td>
+                <td>
+                    <a href=?controller=orderdetail&action=updateOrderForm&Orderdetail_ID=$orderdetail->Orderdetail_ID>Update</a>
+                </td>
+                <td>
+                    <a href=?controller=orderdetaile&action=deleteRateConfirm&Orderdetail_ID=$orderdetail->Orderdetail_ID>Delete</a>
+                </td></tr>";
+            }
+            echo "</table>";
         ?>
-    <a class= "btn btn-success" href=?controller=orderdetail&action=newOrderdetail>AddOrderdetail</a>    
+    <a class= "btn btn-success" href=?controller=orderdetail&action=newOrderdetail>Add</a>
     </div>
     
     
