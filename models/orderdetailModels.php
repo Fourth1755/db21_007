7e8a42a19b38971134821435190bd638c09d5c03
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-10-03 16:46:32 +0700

    Update orderdetailModels.php

--- models/orderdetailModels.php
+++ models/orderdetailModels.php
@@ -54,9 +54,27 @@
             return "Add success $result rows";
 
         }
+        public static function search($key){
+            require("connection_connect.php");
+            $sql = "SELECT QuotationDetail_ID,Quotation_ID,ColorDetail_ID,QuotationDetail_QuantityColor,QuotationDetail_Quantity FROM QuotationDetail
+            WHERE (QuotationDetail_ID LIKE'%$key' OR Quotation_ID LIKE'%$key' OR ColorDetail_ID LIKE'%$key' OR QuotationDetail_QuantityColor LIKE'%$key' OR QuotationDetail_Quantity LIKE '$key')";
+            $result = $conn->query($sql);
+            while($my_row = $result->fetch_assoc()){
+                $Orderdetail_ID = $my_row["QuotationDetail_ID"];
+                $Order_ID = $my_row["Quotation_ID"];
+                $Colordetail_ID = $my_row["ColorDetail_ID"];
+                $Orderdetail_QuantityColor = $my_row["QuotationDetail_QuantityColor"];
+                $Orderdetail_Quantity = $my_row["QuotationDetail_Quantity"];
+
+                $OrderdetailList[] = new Orderdetail($Orderdetail_ID,$Order_ID,$Colordetail_ID,$Orderdetail_QuantityColor,$Orderdetail_Quantity);
+            }
+            require("connection_close.php");
+            return $rateList;
+        }
         public static function update($Orderdetail_ID,$Order_ID,$Colordetail_ID,$Orderdetail_QuantityColor,$Orderdetail_Quantity){
             require("connection_connect.php");
-            $sql="UPDATE QuotationDetail SET QuotationDetail_ID='$Orderdetail_ID',Quotation_ID=$Order_ID,ColorDetail_ID='$Colordetail_ID',QuotationDetail_QuantityColor='$Orderdetail_QuantityColor',QuotationDetail_Quantity='$Orderdetail_Quantity')";
+            $sql="UPDATE QuotationDetail SET QuotationDetail_ID='$Orderdetail_ID',Quotation_ID=$Order_ID,ColorDetail_ID='$Colordetail_ID',QuotationDetail_QuantityColor='$Orderdetail_QuantityColor',QuotationDetail_Quantity='$Orderdetail_Quantity'";
+            $result = $conn->query($sql);
             require("connection_close.php");
             return "Update success $result rows";
         }
