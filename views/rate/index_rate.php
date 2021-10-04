a1dc7684a3ca3e180988cecdf33339ed1b09028f
plaiblindspot <91516531+plaiblindspot@users.noreply.github.com>
2021-10-04 10:37:54 +0700

    Update index_rate.php

--- views/rate/index_rate.php
+++ views/rate/index_rate.php
@@ -9,36 +9,34 @@
 <body >
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-    <div class="container-fluid">
-        <a class="navbar-brand">Rate</a>
-    <form class="d-flex" method="get" action="">
-        <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search" name="key">
-        <input type="hidden" name="controller" value="rate">
-        <button class="btn btn-outline-light" type="submit" value="search" name="action">Search</button>
-      </form>
+            <div class="container-fluid">
+                <a class="navbar-brand">Rate</a>
+                <form class="d-flex" method="get" action="">
+                    <input class="form-control me-2" type="text" placeholder="Search" aria-label="Search" name="key">
+                    <input type="hidden" name="controller" value="rate">
+                    <button class="btn btn-outline-light" type="submit" value="search" name="action">Search</button>
+                </form>
+            </div>
+        </nav>
+            <table class="table">
+            <tr><td>Product_ID</td><td>Product_Name</td><td>Rate_ID</td><td>Rate_Quantity</td><td>Rate_Price</td><td>Rate_ScreenPrice</td><td>Update</td><td>Delete</td></tr>
+            <?php
+                foreach($rateList as $Rate){
+                echo "<tr>
+                    <td>$Rate->Product_ID</td>
+                    <td>$Rate->Product_Name</td>
+                    <td>$Rate->Rate_ID</td>
+                    <td>$Rate->Rate_Quantity</td>
+                    <td>$Rate->Rate_Price</td>
+                    <td>$Rate->Rate_ScreenPrice</td>"?>
+                    <td><a type="button" class="btn btn-outline-primary" href=?controller=rate&action=updateRateForm&<?php echo "Rate_ID=$Rate->Rate_ID";?>>Update</a></td>
+                    <td><a class="btn btn-danger" href=?controller=rate&action=deleteRateConfirm&<?php echo "Rate_ID=$Rate->Rate_ID";?>>Delete</a></td></tr>
+                    <?php
+                }
+                echo "</table>";
+            ?>
+        <a class= "btn btn-success" href=?controller=rate&action=newRate>Add</a>
     </div>
-    </nav>
-        <table class="table">
-        <tr><td>Product_ID</td><td>Product_Name</td><td>Rate_ID</td><td>Rate_Quantity</td><td>Rate_Price</td><td>Rate_ScreenPrice</td><td>Update</td><td>Delete</td></tr>
-        <?php
-            foreach($rateList as $Rate){
-            echo "<tr>
-                <td>$Rate->Product_ID</td>
-                <td>$Rate->Product_Name</td>
-                <td>$Rate->Rate_ID</td>
-                <td>$Rate->Rate_Quantity</td>
-                <td>$Rate->Rate_Price</td>
-                <td>$Rate->Rate_ScreenPrice</td>"?>
-                <td><a type="button" class="btn btn-outline-primary" href=?controller=rate&action=updateRateForm&<?php echo "Rate_ID=$Rate->Rate_ID";?>>Update</a></td>
-                <td><a class="btn btn-danger" href=?controller=rate&action=deleteRateConfirm&<?php echo "Rate_ID=$Rate->Rate_ID";?>>Delete</a></td></tr>
-                <?php
-            }
-            echo "</table>";
-        ?>
-    <a class= "btn btn-success" href=?controller=rate&action=newRate>Add</a>
-    </div>
-    
-    
     
 </body>
 </html>
