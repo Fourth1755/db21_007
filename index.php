3a8198dd9df3b173711f642b840dcf1fdd454b8c
Fourth1755 <fourthe.hero@gmail.com>
2021-10-04 17:55:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <?php 
-    // if(isset($_GET['controller'])&&isset($_GET['action'])){
-    //     $controller = $_GET['controller'];
-    //     $action =$_GET['action'];
-    // }
-    // else{
-    //     $controller='pages';
-    //     $action='home';
-    // }
+     if(isset($_GET['controller'])&&isset($_GET['action'])){
+         $controller = $_GET['controller'];
+         $action =$_GET['action'];
+     }
+     else{
+         $controller='pages';
+         $action='home';
+    }
     ?>
 <!DOCTYPE html>
 <html lang="en">
