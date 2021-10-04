9be1dcf07eaa5a09887d891104f4644f1342e898
LalitaMeaw <91516827+LalitaMeaw@users.noreply.github.com>
2021-10-04 12:33:03 +0700

    1

--- views/orderdetail/updateOrderdetailForm.php
+++ views/orderdetail/updateOrderdetailForm.php
@@ -5,18 +5,24 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>updateOrderdetailForm</title>
-</head>
-        </select></label><br>
-        <label>Product_Name<select name="Product_ID">
-            <?php foreach($order_list as $order){
-                echo "<option value=$order->id";
-                if($order->id == $order->id){echo" selected = 'selected'";}
-                echo ">$order->id</option>";
-                }
-            ?>
+    </head>
+<body>
+<div class="container">
+        <h1>Add new Orderdetail</h1>
         <label>Order_ID<input type="text" name="Order_ID" value="<?php echo $orderdetail->Order_ID;?>"></label><br>
-        <label>Colordetail_ID<input type="text" name="Colordetail_ID" value="<?php echo $orderdetail->Colordetail_ID;?>"></label><br>
-        <form method="get" action="">
+        <form method="get" action="" style="width: 40%; ">
+                <div class="mb-3">
+                    <label for="formGroupExampleInput2" class="form-label">Colordetaildetail
+                        <select name="Colordetail_ID" class="form-select" aria-label="Default select example">
+                                <?php foreach($colordetail_list as $color){
+                                    {
+                                        echo"<option value=$color->Colordetail_ID>$color->Product_ID $color->Color_Name</option>";
+                                    }     
+                                }    
+                                ?>
+                        </select>
+                    </label><br>
+                </div>
         <label>Orderdetail_QuantityColor<input type="text" name="Orderdetail_QuantityColor" value="<?php echo $orderdetail->Orderdetail_QuantityColor;?>"></label><br>
         <label>Orderdetail_Quantity<input type="text" name="Orderdetail_Quantity" value="<?php echo $orderdetail->Orderdetail_Quantity;?>"></label><br>
         
